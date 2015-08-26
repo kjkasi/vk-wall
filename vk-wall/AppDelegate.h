c722b66be9737afaf5726db9838b61d962e058a1
Anton Minin <anton.minin@icloud.com>
2015-08-26 14:39:02 +0300

    Initial commit

---
+++ vk-wall/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  vk-wall
+//
+//  Created by Anton Minin on 26/08/15.
+//  Copyright (c) 2015 Anton Minin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ vk-wall/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  vk-wall
+//
+//  Created by Anton Minin on 26/08/15.
+//  Copyright (c) 2015 Anton Minin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
